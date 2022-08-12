e81144dcaccd9b0314b8469d50a889657d9654df
Henry <henryyu0198@gmail.com>
2022-08-11 21:11:43 -0400

    Add 3 screen files for main view

---
+++ app/src/main/java/com/example/runtime/screens/HomeScreen.kt
@@ -0,0 +1,13 @@
+package com.example.runtime.screens
+
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@Preview
+@Composable
+fun HomeScreen() {
+    Text(
+        text = "Home"
+    )
+}
